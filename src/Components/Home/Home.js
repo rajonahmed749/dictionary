e4fba45b309ae297287a891b1ffb0f20367fada7
rajonahmed749 <raazon749@gmail.com>
2021-07-22 01:34:17 +0600

    navbar added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menubar from '../Shared/Menubar';
+
+const Home = () => {
+    return (
+        <main>
+            <Menubar />
+        </main>
+    );
+};
+
+export default Home;
