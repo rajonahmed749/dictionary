997ccca574a64b47e53a2a7dc6227445518d3d79
rajonahmed749 <raazon749@gmail.com>
2021-09-15 23:42:30 +0600

    material ui added + more modify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from '@material-ui/core';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
-import Home from './Components/Home/Home';
+
 
 
 function App() {
+  const [word, setWord] = useState("")
+  const [meanings, setMeanings] = useState([])
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/plane');
+      console.log("actual data", data);
+      setMeanings(data.data)
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log("from state", meanings);
+
+  useEffect(() => {
+    dictionaryApi()
+  }, [])
+
   return (
-    <div>
-      <Home />
+    <div style={{ height: '100vh', backgroundColor: '#282c34' ,color:'white'}}>
+      <Container maxWidth="md" >
+        the dictionary app
+    </Container>
     </div>
   );
 }
