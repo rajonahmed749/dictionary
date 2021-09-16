5f9c52562a9642b821633490146f5f2aea37b0ae
rajonahmed749 <raazon749@gmail.com>
2021-09-16 21:57:10 +0600

    audio system added

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -4,10 +4,21 @@ const Definitions = ({ category, meanings, word }) => {
     return (
         <div className="meanings">
             {
+                meanings[0] && word && category === 'en' && (
+                    <audio
+                        src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+                        style={{ backgroundColor: "#fff", borderRadius: 10 }}
+                        controls
+                    >
+                        your browser doesn't support audio element
+                    </audio>
+                )
+            }
+            {
                 word === "" ? (<span className="subTitle">Start by typing a word in search</span>) : (
                     meanings.map((mean) => mean.meanings.map((item) => (
                         item.definitions.map((def) => (
-                            <div className="signleMean" style={{ backgroundColor: "white", color: "black" }}>
+                            <div className="singleMean" style={{ backgroundColor: "white", color: "black" }}>
                                 <b>{def.definition}</b>
                                 <hr style={{ backgroundColor: "black", width: "100%" }} />
                                 {
