b4d90d716a418e6be861137a03f3cc1e9819b599
rajonahmed749 <raazon749@gmail.com>
2021-09-18 21:29:05 +0600

    context api created

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import Learning from "./components/Learning/Learning";
 export const UserContext = createContext()
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({})
   const [LightMode, setLightMode] = useState(false);
   const DarkMode = withStyles({
     switchBase: {
@@ -32,47 +33,49 @@ function App() {
     track: {},
   })(Switch);
   return (
-    <Router>
-      <div style={{
-        minHeight: "100vh",
-        backgroundColor: LightMode ? "#fff" : '#0a192f',
-        color: LightMode ? "black" : 'white',
-        transition: "all 0.5s linear"
-      }}>
-        <div className="menubar" style={{ top: 0, paddingTop: 10 }}>
-          {/* Navbar */}
-          <div>
-            <Link to="/">
-              <i style={{ color: LightMode ? "black" : "white" }} className="fas fa-home icons fa-lg"></i>
-            </Link>
-            <Link to="/addWord">
-              <i style={{ color: LightMode ? "black" : "white" }} className="fas fa-chalkboard-teacher icons fa-lg"></i>
-            </Link>
-            <Link to="/learning">
-              <i style={{ color: LightMode ? "black" : "white" }} className="fab fa-leanpub icons fa-lg"></i>
-            </Link>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <div style={{
+          minHeight: "100vh",
+          backgroundColor: LightMode ? "#fff" : '#0a192f',
+          color: LightMode ? "black" : 'white',
+          transition: "all 0.5s linear"
+        }}>
+          <div className="menubar" style={{ top: 0, paddingTop: 10 }}>
+            {/* Navbar */}
+            <div>
+              <Link to="/">
+                <i style={{ color: LightMode ? "black" : "white" }} className="fas fa-home icons fa-lg"></i>
+              </Link>
+              <Link to="/addWord">
+                <i style={{ color: LightMode ? "black" : "white" }} className="fas fa-chalkboard-teacher icons fa-lg"></i>
+              </Link>
+              <Link to="/learning">
+                <i style={{ color: LightMode ? "black" : "white" }} className="fab fa-leanpub icons fa-lg"></i>
+              </Link>
+            </div>
+            <div>
+              <span>{LightMode ? "Dark" : "Light"} Mode</span>
+              <DarkMode
+                checked={LightMode} onChange={() => setLightMode(!LightMode)}
+              />
+            </div>
           </div>
-          <div>
-            <span>{LightMode ? "Dark" : "Light"} Mode</span>
-            <DarkMode
-              checked={LightMode} onChange={() => setLightMode(!LightMode)}
-            />
-          </div>
+          <ChangePath>
+            <Route exact path="/">
+              <Home LightMode={LightMode} />
+            </Route>
+            <Route path="/addWord">
+              <Profile />
+            </Route>
+            <Route path="/learning">
+              <Learning LightMode={LightMode} />
+            </Route>
+          </ChangePath>
+          <Footer LightMode={LightMode} />
         </div>
-        <ChangePath>
-          <Route exact path="/">
-            <Home LightMode={LightMode} />
-          </Route>
-          <Route path="/addWord">
-            <Profile />
-          </Route>
-          <Route path="/learning">
-            <Learning LightMode={LightMode} />
-          </Route>
-        </ChangePath>
-        <Footer LightMode={LightMode} />
-      </div>
-    </Router>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
